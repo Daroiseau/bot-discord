d97f0a0a21dce617ace16c44e14895160e202c6f
Pablo <tanguymonguillon@gmail.com>
2025-03-31 13:57:17 +0200

    update bd.js2

--- database/bd.js
+++ database/bd.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'bot_discord_bdd_user',
+  user: 'bddpablo_user',
   host: 'dpg-cvl81bffte5s739tcta0-a.frankfurt-postgres.render.com',
-  database: 'bot_discord_bdd',
+  database: 'bddpablo',
   password: 'VOiMwZADHc23C6dNZyfFmfIqSrcn2gJa',
   port: 5432,
   ssl: {
