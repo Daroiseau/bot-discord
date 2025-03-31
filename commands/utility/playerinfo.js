919d418110a218589e96e9b27bf2e25b7579cdb0
Pablo <tanguymonguillon@gmail.com>
2025-03-31 13:48:54 +0200

    ajout fct player info et remise a jour de la bdd

--- commands/utility/playerinfo.js
+++ commands/utility/playerinfo.js
@@ -40,3 +40,37 @@ async function getSummonerInfo(summonerName, tag) {
         return null;
     }
 }
+
+function createGameResultsEmbed(m_data){
+    const embed = new EmbedBuilder()
+    .setAuthor({
+        name : `Pablo`,
+        iconURL: 'https://cdn.discordapp.com/attachments/1220074375093420142/1316399558108123177/ppDiscord.png?ex=675ae820&is=675996a0&hm=52a4de52b8f8e2cec96a0c6712b251cf121b67ed31f38f9a9af61108cd40d42f&',
+    })
+    .setTitle(m_data.gameStatue === "win" ? 'Victory' : 'Defeat')
+    .setDescription( m_data.promotion === "up" ? `${m_data.pseudo} a ${m_data.gameStatue} ${m_data.lp} league points et a été promu ${m_data.tier} ${m_data.rank} avec ${m_data.lpGeneral}  lp` 
+                    : m_data.promotion ==="down" ? `${m_data.pseudo} a ${m_data.gameStatue} ${m_data.lp} league points et a été relégué ${m_data.tier} ${m_data.rank} avec ${m_data.lpGeneral}  lp`
+                    :`${m_data.pseudo} a ${m_data.gameStatue} ${m_data.lp} league points et arrive ${m_data.tier} ${m_data.rank} ${m_data.lpGeneral}  lp`)
+    .setColor(m_data.color || '#ffffff') // Couleur de l'embed
+    .setThumbnail(`https://ddragon.leagueoflegends.com/cdn/14.23.1/img/champion/${m_data.champion}.png`)
+    // Ajouter des champs (fields)
+    .addFields(
+        {
+          name: "score",
+          value: m_data.kills+'/'+m_data.deaths+'/'+m_data.assists || "non disponible",
+          inline: true
+        },
+        {
+          name: "champion",
+          value:  m_data.champion || "Non disponible",
+          inline: true
+        },
+        {
+          name: "queue",
+          value: m_data.queue || "Non disponible",
+          inline: true
+        },
+      );
+    
+    return embed;
+}
--- database/bd.js
+++ database/bd.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'bot_discord_bdd_user',
-  host: 'dpg-ctuh1123esus739c7350-a.frankfurt-postgres.render.com',
+  host: 'dpg-cvl81bffte5s739tcta0-a.frankfurt-postgres.render.com',
   database: 'bot_discord_bdd',
   password: '0SAktUZCKrbCncs5IyRMPNdz3ort0a9j',
   port: 5432,
