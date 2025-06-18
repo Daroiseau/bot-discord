cf498569c4ee94114d32a5e31937ab5824aa472e
Pablo <tanguymonguillon@gmail.com>
2025-06-18 16:06:03 +0200

    Update bd.js

--- database/bd.js
+++ database/bd.js
@@ -2,15 +2,14 @@ import pkg from 'pg';
 const {Pool} = pkg;
 
 const pool = new Pool({
-  user: 'bddpablo_user',
-  host: 'dpg-cvl81bffte5s739tcta0-a.frankfurt-postgres.render.com',
-  database: 'bddpablo',
-  password: 'VOiMwZADHc23C6dNZyfFmfIqSrcn2gJa',
+  user: 'postgres',
+  host: 'postgres.railway.internal',
+  database: 'railway',
+  password: 'LiNfHIMmyivSUeYBzTZlmkYTqPLUZiKr',
   port: 5432,
-  ssl: true //pour render 
- /* ssl: {
+  ssl: {
     rejectUnauthorized: false
-  }*/
+  }
 });
 
 export default pool;
