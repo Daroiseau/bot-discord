6875f1ed396b3483866160cda2cdf39838afe998
Pablo <tanguymonguillon@gmail.com>
2025-07-17 15:40:46 +0200

    update fct verifypuuid

--- lpTracker/lptracker.js
+++ lpTracker/lptracker.js
@@ -76,7 +76,7 @@ async function verifPuuid(summonerName, tag, puuid, riotAPIKey ) {
         if(response.data.puuid === puuid){
             return puuid;
         }
-        await updateData('lol_accounts', {puuid : response.data.puuid}, {game_name : summonerName, tag : tag});
+        await updateData('lol_accounts', {puuid : response.data.puuid}, {puuid : puuid});
         return response.data.puuid;
     } catch (error) {
         console.error('Erreur lors de la vérification du PUUID :', error);
